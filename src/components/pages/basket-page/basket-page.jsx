6e345a0deddaceabd01fc182f3dfecb980e4a575
Aleksandra <77754618083@mail.ru>
2022-04-18 20:55:19 +0300

    Закончила верстку

--- src/components/pages/basket-page/basket-page.jsx
+++ src/components/pages/basket-page/basket-page.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./basket-page.scss";
 import Basket from "../../basket/basket";
 import Logo from "../../logo/logo";
+import Empty from "../../empty/empty";
 
 const BasketPage = () => {
   return (
     <>
       <Logo className="basket-logo" />
       <Basket />
+      {/* <Empty /> */}
     </>
   );
 };
